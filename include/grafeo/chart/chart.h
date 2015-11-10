cfc979d708d43463ff11a3013a15d70a829723fe
Anderson Tavares <nocturne.pe@gmail.com>
2015-11-10 17:45:16 -0200

    Tentando portabilidade

--- include/grafeo/chart/chart.h
+++ include/grafeo/chart/chart.h
@@ -32,6 +32,30 @@
 #include <glib-object.h>
 
 #define GRF_TYPE_CHART grf_chart_get_type()
+
+#if !GLIB_CHECK_VERSION(2,44,0)
+  #define G_DECLARE_DERIVABLE_TYPE(ModuleObjName, module_obj_name, MODULE, OBJ_NAME, ParentName) \
+    GType module_obj_name##_get_type (void);                                                               \
+    G_GNUC_BEGIN_IGNORE_DEPRECATIONS                                                                       \
+    typedef struct _##ModuleObjName ModuleObjName;                                                         \
+    typedef struct _##ModuleObjName##Class ModuleObjName##Class;                                           \
+    struct _##ModuleObjName { ParentName parent_instance; };                                               \
+                                                                                                           \
+    _GLIB_DEFINE_AUTOPTR_CHAINUP (ModuleObjName, ParentName)                                               \
+                                                                                                           \
+    static inline ModuleObjName * MODULE##_##OBJ_NAME (gconstpointer ptr) {                                     \
+      return G_TYPE_CHECK_INSTANCE_CAST (ptr, module_obj_name##_get_type (), ModuleObjName); }             \
+    static inline ModuleObjName##Class * MODULE##_##OBJ_NAME##_CLASS (gconstpointer ptr) {                      \
+      return G_TYPE_CHECK_CLASS_CAST (ptr, module_obj_name##_get_type (), ModuleObjName##Class); }         \
+    static inline gboolean MODULE##_IS_##OBJ_NAME (gconstpointer ptr) {                                         \
+      return G_TYPE_CHECK_INSTANCE_TYPE (ptr, module_obj_name##_get_type ()); }                            \
+    static inline gboolean MODULE##_IS_##OBJ_NAME##_CLASS (gconstpointer ptr) {                                 \
+      return G_TYPE_CHECK_CLASS_TYPE (ptr, module_obj_name##_get_type ()); }                               \
+    static inline ModuleObjName##Class * MODULE##_##OBJ_NAME##_GET_CLASS (gconstpointer ptr) {                  \
+      return G_TYPE_INSTANCE_GET_CLASS (ptr, module_obj_name##_get_type (), ModuleObjName##Class); }       \
+    G_GNUC_END_IGNORE_DEPRECATIONS
+#endif
+
 G_DECLARE_DERIVABLE_TYPE(GrfChart, grf_chart, GRF, CHART, GrfChartContainer)
 
 typedef struct _GrfChartClass{
@@ -165,7 +189,7 @@ char*
 grf_chart_panel_get_title(GrfChartPanel* chart_panel);
 /**
  * @brief grf_chart_get_last_panel
- * @param chart
+ * @param chartwercker newer ubuntu
  * @return
  */
 GrfChartPanel*
